718d549595f74a5e85efd0a2aadd53295d80022f
ektabhatt10 <ektabhatt10@gmail.com>
2025-10-27 16:05:22 +0100

    Update Optimizer.py

--- Optimizer.py
+++ Optimizer.py
@@ -6,8 +6,6 @@ def run_optimization(parcels_df, containers_df, settings, sharing_allowed):
     import numpy as np
 
     # Read Excel files
-    parcels_df = pd.read_excel("parcels.xlsx")
-    containers_df = pd.read_excel("containers.xlsx")
 
     # Preview the data
     print("Parcels:")
